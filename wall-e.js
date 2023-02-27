4beda23e96cd51237074fd4fcb48eb55b5c13b79
TildeMarkstrom <tilde_markstrom@hotmail.com>
2023-02-27 23:24:33 +0100

    wall-e done

--- wall-e.js
+++ wall-e.js
@@ -3,21 +3,45 @@ background(255);
 
 
 //wheel right + left)
-fill(40,40,40);
+fill(50);
 strokeWeight(4);
-stroke(20,20,20);
-rect(98, 290, 50, 90);
+stroke(40);
+rect(108, 260, 50, 100);
 
-rect(332, 290, 50, 90);
+line(108, 268, 158, 268);
+line(108, 277, 158, 277);
+line(108, 286, 158, 286);
+line(108, 295, 158, 295);
+line(108, 304, 158, 304);
+line(108, 313, 158, 313);
+line(108, 322, 158, 322);
+line(108, 331, 158, 331);
+line(108, 340, 158, 340);
+line(108, 349, 158, 349);
+line(108, 358, 158, 358);
+
+rect(322, 260, 50, 100);
+
+line(323, 268, 368, 268);
+line(323, 277, 368, 277);
+line(323, 286, 368, 286);
+line(323, 295, 368, 295);
+line(323, 304, 368, 304);
+line(323, 313, 368, 313);
+line(323, 322, 368, 322);
+line(323, 331, 368, 331);
+line(323, 340, 368, 340);
+line(323, 349, 368, 349);
+line(323, 358, 368, 358);
 
 //wheel connections
-rect(150, 338, 40, 10);
-rect(195, 327, 10, 10);
-ellipse(200, 343, 20);
+rect(160, 318, 40, 10);
+rect(200, 307, 10, 10);
+ellipse(205, 325, 20);
 
-rect(290, 338, 40, 10);
-rect(275, 327, 10);
-ellipse(280, 343, 20);
+rect(280, 318, 40, 10);
+rect(265, 307, 10);
+ellipse(270, 325, 20);
 
 //Cords to the eyes
 beginShape();
@@ -55,7 +79,7 @@ rect(236,100,8,20);
 rect(220,80, 40,20);
 
 
-//eyes
+//eye right 
 push();
 
 translate(255, 65);
@@ -73,9 +97,13 @@ bezierVertex(30.15, 47.31, 1.16, 45.46, -7.65, 19.95);
 bezierVertex(-7.65, 19.95, -8.11, 8.44, 0, 0);
 endShape();
 
+//camera eye
 fill(50, 10, 40);
 ellipse(15, 23, 25);
+fill(20);
+ellipse(15,23, 10);
 
+//eye screws
 fill(40);
 strokeWeight(1);
 ellipse(2,8,4);
@@ -85,6 +113,8 @@ ellipse(30, 40, 4);
 pop();
 
 push();
+
+//eye left
 
 translate(225, 65);
 scale(1.2);
@@ -101,9 +131,13 @@ bezierVertex(-30.15, 47.31, -1.16, 45.46, 7.65, 19.95);
 bezierVertex(7.65, 19.95, 8.11, 8.44, 0, 0);
 endShape();
 
+//camera eye left
 fill(50, 10, 40);
 ellipse(-15, 23, 25);
+fill(20);
+ellipse(-15,23, 10);
 
+//eye screws left
 strokeWeight(1);
 fill(40);
 ellipse(-2, 8, 4);
@@ -117,6 +151,17 @@ fill(255,197,28);
 strokeWeight(2);
 stroke(235,177,18);
 rect(160,150,160,160);
+
+//box-body shadows
+push();
+stroke(205,150,0);
+strokeWeight(5);
+noFill();
+
+line(170, 201, 170, 254);
+line(240, 201, 240, 254);
+line(310, 201, 310, 254);
+pop();
 
 //box-body details
 stroke(60);
@@ -197,13 +242,95 @@ fill(255,197,28);
 
 ellipse(5, 10, 10);
 
-push();
 rotate(320);
 rect(0,0, 100, 20);
+fill(50);
+stroke(50);
+rect(0, 15, 100, 5);
 pop();
 
+//hand right
+push();
+translate(401,146);
+angleMode(DEGREES);
+rotate(320);
 
+rect(0,0, 20,10);
+pop();
 
+push();
+translate(405,130);
+rotate(320);
+scale(1.5);
+strokeWeight(1);
+beginShape();
+vertex(0,0);
+vertex(0,20);
+vertex(35,20);
+vertex(35, 12);
+vertex(20, 10);
+vertex(35, 8);
+vertex(35, 0);
+vertex(0,0);
+endShape();
+
+beginShape();
+vertex(0, 5);
+vertex(0, 15);
+vertex(20, 15);
+vertex(20, 5);
+vertex(0,5);
+endShape();
 pop();
 
 //arm left
+push();
+
+angleMode(DEGREES);
+translate(145, 224);
+
+stroke(235,177,18);
+fill(255,197,28);
+
+ellipse(10, -5, 10);
+
+rotate(220);
+rect(0,0, 100, 20);
+fill(50);
+stroke(50);
+rect(0, 0, 100, 5);
+pop();
+
+//hand left
+push();
+translate(71,156);
+angleMode(DEGREES);
+rotate(220);
+
+rect(0,0, 20,10);
+pop();
+
+push();
+translate(50,154);
+rotate(220);
+scale(1.5);
+strokeWeight(1);
+beginShape();
+vertex(0,0);
+vertex(0,20);
+vertex(35,20);
+vertex(35, 12);
+vertex(20, 10);
+vertex(35, 8);
+vertex(35, 0);
+vertex(0,0);
+endShape();
+
+beginShape();
+vertex(0, 5);
+vertex(0, 15);
+vertex(20, 15);
+vertex(20, 5);
+vertex(0,5);
+endShape();
+pop();
