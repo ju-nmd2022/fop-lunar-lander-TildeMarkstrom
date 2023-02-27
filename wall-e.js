b930827bb473ecb85abe2df85025e1d0266830c9
TildeMarkstrom <mati22yl@student.ju.se>
2023-02-27 16:49:22 +0100

    Create wall-e.js

---
+++ wall-e.js
@@ -0,0 +1,42 @@
+background(255);
+
+
+
+//wheel right + left)
+fill(40,40,40);
+strokeWeight(4);
+stroke(20,20,20);
+rect(98, 290, 50, 90);
+
+rect(332, 290, 50, 90);
+
+//wheel connections
+rect(150, 338, 40, 10);
+rect(195, 327, 10, 10);
+ellipse(200, 343, 20);
+
+rect(290, 338, 40, 10);
+rect(275, 327, 10);
+ellipse(280, 343, 20);
+
+//neck
+fill(220,220,0);
+strokeWeight(1);
+stroke(180,180,0);
+rect(230,110, 20, 40);
+
+rect(230,125, 20, 10);
+
+//eyes
+noFill();
+stroke(0);
+strokeWeight(1);
+line(247,111,247,99);
+
+
+//box
+fill(255,255,0);
+strokeWeight(1);
+stroke(180,180,0);
+rect(150,150,180,180);
+
